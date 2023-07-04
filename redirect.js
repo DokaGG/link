592a72f536415056ed4cf4a6853add5be82b08c4
DokaGG <57476039+DokaGG@users.noreply.github.com>
2023-07-04 21:49:27 +0530

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,28 @@
+// Define the destination URLs
+var destinationURLs = {
+    "http://127.0.0.1:5500/link1": "www.destination1.url/example1",
+    "http://127.0.0.1:5500/link2": "www.destination2.url/example2",
+    "http://127.0.0.1:5500/link3": "www.destination3.url/example3"
+  };
+  
+  // Get the current URL
+  var currentURL = window.location.href;
+  
+  // Function to perform the redirect based on the current URL
+  function redirect() {
+    // Check if the current URL exists in the destination URLs
+    if (destinationURLs.hasOwnProperty(currentURL)) {
+      // Get the corresponding destination URL
+      var destinationURL = destinationURLs[currentURL];
+      
+      // Redirect the user to the destination URL
+      window.location.href = destinationURL;
+    } else {
+      // If the current URL is not found in the destination URLs, handle it as needed
+      console.log("Invalid short URL");
+    }
+  }
+  
+  // Delay the redirect for 3 seconds (adjust as needed)
+  setTimeout(redirect, 3000);
+  
